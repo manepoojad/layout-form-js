9392f6945b3ab964073fa859733e154c07d59425
Pooja Mane <manepoojad@gmail.com>
2023-03-28 20:19:54 +0530

    event listener added

---
+++ 04-/formCRUD.js
@@ -0,0 +1,7 @@
+// formSubmit
+document.getElementById("formSubmit").addEventListener("click", displayDate);
+
+function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+console.log("function call")
+}
