ad612fad0f771356f1b3f0bde66e2299095edd3a
Pooja Mane <manepoojad@gmail.com>
2023-03-29 20:34:11 +0530

    formcrud added

--- 04-/formCRUD.js
+++ 04-/formCRUD.js
@@ -1,7 +1,38 @@
-// formSubmit
 document.getElementById("formSubmit").addEventListener("click", displayDate);
 
 function displayDate() {
-//   document.getElementById("demo").innerHTML = Date();
-console.log("function call")
+    let firstNameValue = document.getElementById("firstName").value;
+
+    let lastNameValue = document.getElementById("lastName").value;
+
+    let dateOfBirthValue = document.getElementById("dateOfBirth").value;
+
+    let genderElement = document.querySelector('input[name = gender]:checked');
+    let genderValue = ""
+    if (genderElement) {
+        genderValue = genderElement.value;
+    }
+
+    let checkboxes = document.querySelectorAll('input[name="knownLanguage"]:checked');
+    let knownLanguage = [];
+    checkboxes.forEach((checkbox) => {
+        knownLanguage.push(checkbox.value);
+    });
+
+    let courseValue = document.getElementById("course").value;
+
+    let addressValue = document.getElementById("address").value;
+
+    const formData = {
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        dateOfBirth: dateOfBirthValue,
+        gender: genderValue,
+        course: courseValue,
+        knownLanguage: knownLanguage,
+        address: addressValue
+
+
+    }
+    console.log("formdata", formData)
 }
