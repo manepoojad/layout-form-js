ecf531d92e210de1424150873562f983aca8bded
Pooja Mane <manepoojad@gmail.com>
2023-04-01 16:25:43 +0530

    added

---
+++ 05-dom-manipulation/functionVariable.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log("This is test file")
+let fruit = "apple";
+// console.log("fruit",fruit)
+const special = fruit
+// console.log("special",special)
+
+
+
+
+
+
+test()
+function test() {
+    console.log("This is function test")
+}
+// test()
+
+const myTest = () => {
+    console.log("This is arrow function myTest")
+}
+// myTest()
+
