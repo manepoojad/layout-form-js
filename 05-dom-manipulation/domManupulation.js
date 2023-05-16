2c774e3a4e0b1206d75f54a86dbe034f31e90436
Pooja Mane <manepoojad@gmail.com>
2023-05-16 23:29:25 +0530

    changes added

--- 05-dom-manipulation/domManupulation.js
+++ 05-dom-manipulation/domManupulation.js
@@ -2,7 +2,7 @@ document.getElementById("add-new-data").addEventListener("click", appendForm);
 
 function appendForm() {
 
-    document.getElementById('main').innerHTML = `<h2 class="form-heading">Student Form</h2>
+    document.getElementById('main').innerHTML = `<h2 class="form-heading">Add New Student Data</h2>
     <form class="form-section">
         <div class="formfield">
             <label class="formlabel" for="firstName">First name:</label>
@@ -53,9 +53,7 @@ function appendForm() {
     </form>
         `
     document.getElementById("formSubmit").addEventListener("click", getFormData);
-
 }
-
 document.getElementById("get-data").addEventListener("click", getData);
 
 function getData() {
@@ -95,16 +93,18 @@ function getData() {
            <td>${dataObject.knownLanguage}</td>
            <td>${dataObject.address}</td>
            <td>${dataObject.course}</td>
-           <td><button onclick="deleteRecord(${i})">Delete</button></td>
+           <td>
+           <button onclick="deleteRecord(${i})">Delete</button>
+           <button onclick="editRecord(${i})">Edit</button>
+           </td>
+          
          </tr>`
 
     }
 
-
     document.getElementById("tableBody").innerHTML = tableBodyData
 }
 getData()
-
 
 function getFormData(e) {
     e.preventDefault()
@@ -119,16 +119,13 @@ function getFormData(e) {
     if (genderElement) {
         genderValue = genderElement.value;
     }
-
     let checkboxes = document.querySelectorAll('input[name="knownLanguage"]:checked');
     let knownLanguage = [];
     checkboxes.forEach((checkbox) => {
         knownLanguage.push(checkbox.value);
     });
 
-    // debugger
     let courseValue = document.getElementById("course").value;
-
     let addressValue = document.getElementById("address").value;
 
     const formData = {
@@ -142,15 +139,12 @@ function getFormData(e) {
     }
 
     const localStringifyDataList = localStorage.getItem("localDataList")
-
-
     const dataList = JSON.parse(localStringifyDataList)
     dataList.push(formData)
     const stringifyDataList = JSON.stringify(dataList)
     localStorage.setItem("localDataList", stringifyDataList)
     getData()
 }
-
 
 function deleteRecord(index) {
     const allRecordsString = localStorage.getItem("localDataList")
@@ -164,15 +158,146 @@ function deleteRecord(index) {
             return false;
 
         }
-
     }
     const newRecordList = parsedDataList.filter(callBack)
     const newStringifyRecordList = JSON.stringify(newRecordList)
     localStorage.setItem("localDataList", newStringifyRecordList)
     getData()
-
-
 }
+
+function editRecord(index) {
+    const allRecordsString = localStorage.getItem("localDataList")
+    const parsedDataList = JSON.parse(allRecordsString)
+
+    const selectedRecord = parsedDataList[index];
+    console.log(selectedRecord)
+
+    document.getElementById('main').innerHTML = `<h2 class="form-heading">Update Student Data</h2>
+    <form class="form-section">
+        <div class="formfield">
+            <label class="formlabel" for="firstName">First name:</label>
+            <input type="text" id="firstName" name="firstName" placeholder="Enter first Name">
+        </div>
+        <div class="formfield">
+            <label class="formlabel" for="lastName">Last name:</label>
+            <input type="text" id="lastName" name="lastName" placeholder="Enter last Name"><br>
+        </div>
+        <div class="formfield">
+            <label class="formlabel" for="dateOfBirth">Date of Birth:</label>
+            <input type="date" id="dateOfBirth" name="dateOfBirth" placeholder="yyyy-mm-dd">
+        </div>
+        <div class="formfield">
+            <label class="formlabel" for="gender">Gender:</label>
+            <label for="male">Male</label>
+            <input type="radio" id="male" name="gender" value="male">
+            <label for="female">Female</label>
+            <input type="radio" id="female" name="gender" value="female">
+            <label for="other">Other</label>
+            <input type="radio" id="other" name="gender" value="other"><br>
+        </div>
+        <div class="formfield">
+            <label class="formlabel" for="knownLanguage">Known Language:</label>
+            <label for="english">English</label>
+            <input type="checkbox" id="english" name="knownLanguage" value="english">
+            <label for="marathi">Marathi</label>
+            <input type="checkbox" id="marathi" name="knownLanguage" value="marathi">
+            <label for="hindi">Hindi</label>
+            <input type="checkbox" id="hindi" name="knownLanguage" value="hindi">
+        </div>
+        <div class="formfield">
+            <label class="formlabel" for="address">Address:</label>
+            <textarea id="address" name="address" placeholder="Enter address"></textarea>
+        </div>
+        <div class="formfield">
+            <label class="formlabel" for="course">Course Applied for:</label>
+            <select name="course" id="course">
+                <option value="science">Science</option>
+                <option value="commerce">Com</option>
+                <option value="art">Art</option>
+            </select><br>
+        </div>
+        <div class="form-button">
+            <input type="submit" id="formSubmit" value="Submit">
+            <input type="Reset" value="Reset">
+        </div>
+    </form>
+        `
+    document.getElementById("formSubmit").addEventListener("click", (e) => { updateFormData(e, index) });
+
+    document.getElementById("firstName").value = selectedRecord.firstName
+    document.getElementById("lastName").value = selectedRecord.lastName
+    document.getElementById("dateOfBirth").value = selectedRecord.dateOfBirth
+    document.getElementById("course").value = selectedRecord.course
+    document.getElementById("address").value = selectedRecord.address
+
+    function addGenderValueInForm() {
+        const genderValue = selectedRecord.gender
+        if (genderValue == "male") {
+            document.getElementById("male").checked = true
+        }
+        else if (genderValue == "female") {
+            document.getElementById("female").checked = true
+        }
+        else if (genderValue == "other") {
+            document.getElementById("other").checked = true
+        }
+    }
+    addGenderValueInForm()
+
+    function addLangaugeValueInForm() {
+
+        const langaugeValues = selectedRecord.knownLanguage
+        langaugeValues.forEach((element) => {
+            if (element == "english") {
+                document.getElementById("english").checked = true
+            }
+            else if (element == "marathi") {
+                document.getElementById("marathi").checked = true
+            }
+            else if (element == "hindi") {
+                document.getElementById("hindi").checked = true
+            }
+
+        })
+    }
+    addLangaugeValueInForm()
+}
+
+function updateFormData(e, index) {
+    e.preventDefault()
+    let firstNameValue = document.getElementById("firstName").value;
+    let lastNameValue = document.getElementById("lastName").value;
+    let dateOfBirthValue = document.getElementById("dateOfBirth").value;
+    let genderElement = document.querySelector('input[name = gender]:checked');
+    let genderValue = ""
+    if (genderElement) {
+        genderValue = genderElement.value;
+    }
+    let checkboxes = document.querySelectorAll('input[name="knownLanguage"]:checked');
+    let knownLanguage = [];
+    checkboxes.forEach((checkbox) => {
+        knownLanguage.push(checkbox.value);
+    });
+    let courseValue = document.getElementById("course").value;
+    let addressValue = document.getElementById("address").value;
+
+    const formData = {
+        firstName: firstNameValue,
+        lastName: lastNameValue,
+        dateOfBirth: dateOfBirthValue,
+        gender: genderValue,
+        course: courseValue,
+        knownLanguage: knownLanguage,
+        address: addressValue
+    }
+    const localStringifyDataList = localStorage.getItem("localDataList")
+    const dataList = JSON.parse(localStringifyDataList)
+    dataList[index] = formData
+    const stringifyDataList = JSON.stringify(dataList)
+    localStorage.setItem("localDataList", stringifyDataList)
+    getData()
+}
+
 
 
 
--- 05-dom-manipulation/functionVariable.js
+++ 05-dom-manipulation/functionVariable.js
@@ -1,31 +1,7 @@
-"use strict";
-console.log("This is test file")
-let fruit = "banana";
-// console.log("fruit",fruit)
-const special = fruit
-// console.log("special",special)
-
-
-
-
-
-
-// test()
-function test(myVariable) {
-    console.log("This is function test",myVariable)
-}
-test(fruit)
-
-const myTest = () => {
-    console.log("This is arrow function myTest")
-
-    return "pooja"
+function testFunction(params) {
+  const fruit = "apple"   
+  const bird = "Pickok"
 }
 
- const myTestValue = myTest()
- console.log("myTestValue",myTestValue)
-
- 
- 
-
-
+const testValue = testFunction()
+console.log("testValue", testValue)
