6e746555866457f1a7610509fb8dac283c314329
Pooja Mane <manepoojad@gmail.com>
2023-05-27 15:32:39 +0530

    functionconcept

--- 05-dom-manipulation/functionVariable.js
+++ 05-dom-manipulation/functionVariable.js
@@ -1,10 +1,36 @@
-function testFunction(params) {
-  const fruit = "apple"
-  const bird = "Peacock"
+"use strict";
+//IMP function return statement
+function learnReturnStatement() {
+  const fullName = "Pooja Mane"
+  return fullName
 }
 
-const testValue = testFunction()
-console.log("testValue", testValue)
+const returnValue = learnReturnStatement()
+console.log("returnValue", returnValue)
+
+// parameter passed return statement
+function parameterReturnFunction(parameter1, parameter2) {
+  const newObj = {
+    argument1: parameter1,
+    argument2: parameter2,
+  }
+  return newObj
+}
+const fruitName = "banana"
+const parameterReturnValue = parameterReturnFunction("apple", fruitName)
+console.log("parameterReturnValue", parameterReturnValue)
+
+
+const learnArrowFunction = () => {
+  console.log("This is arrow function myTest")
+
+  return "pooja"
+}
+
+const learnArrowFunctionValue = learnArrowFunction()
+console.log("learnArrowFunctionValue", learnArrowFunctionValue)
+
+
 
 const myPromise = new Promise(function (myResolve, myReject) {
   setTimeout(function () { myResolve("I love You !!"); }, 5000);
@@ -12,9 +38,10 @@ const myPromise = new Promise(function (myResolve, myReject) {
 
 console.log(myPromise)
 myPromise.then(function (value) {
-  console.log("promise value",value)
+  console.log("promise value", value)
 });
 console.log(myPromise)
+
 
 
 
